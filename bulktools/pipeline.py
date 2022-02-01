eecd8f755fef1ec8d3dcda9e0a23a59cbe46f3a0
LouisFaure <louis.faure75@gmail.com>
2022-02-01 12:21:59 +0100

    better error and processes handling

--- bulktools/pipeline.py
+++ bulktools/pipeline.py
@@ -17,6 +17,7 @@ import argparse
 from argparse import RawTextHelpFormatter
 from functools import partial
 from shutil import which
+import concurrent.futures
 
 import logging
 from rich.logging import RichHandler
@@ -29,18 +30,19 @@ log = logging.getLogger("rich")
 
 fqcounter = os.path.join(__path__[0], "fqcount")
 
+manager = Manager()
+return_dict = manager.dict()
+
 help = 'Full bulk pipeline, from fastq to adata count matrix!\n'\
     'Performs the following: fastq -STAR-> bam -featureCounts-> anndata.h5ad'
 
 parser = argparse.ArgumentParser(description=help,formatter_class=RawTextHelpFormatter)
-parser.add_argument("--fq_path", help="Path for input fastq files (relative, default: fastq).")
-#parser.add_argument("--bam_path", help="Path for STAS (default: fastq).")
-parser.add_argument("--star_ref", help="STAR index path.")
-parser.add_argument("--gtf", help="GTF file path for featureCounts.")
+parser.add_argument("--fq_path","-f", help="Path for input fastq files (relative, default: fastq).")
+parser.add_argument("--bam_path","-b", help="Path for aligned BAMs (default: aligned).")
+parser.add_argument("--star_ref","-s", help="STAR index path.")
+parser.add_argument("--gtf","-g", help="GTF file path for featureCounts.")
 parser.add_argument("--n_threads","-n", help="number of threads per fastq file, for both STAR and featureCounts.")
-parser.add_argument("--adata_out", help="Path for the adata output (relative, default: adata_bulk_star.h5ad).")
-
-
+parser.add_argument("--adata_out","-o", help="Path for the adata output (relative, default: adata_bulk_star.h5ad).")
 
 def ref_loader(star_ref):
     loadref="STAR --genomeLoad LoadAndExit --genomeDir %s" % star_ref
@@ -60,15 +62,29 @@ def runcom(c):
     out=subprocess.check_output(c.split())
     return int(out)
    
-def run_star(n_threads,star_ref,fq_path,sample):
+def run_star(n_threads,star_ref,fq_path,bam_path,sample):
     fqs=" ".join(glob(os.path.join(fq_path,sample+"*.gz")))
     
-    runstar="STAR --runThreadN %s --limitBAMsortRAM 10000000000 --genomeLoad LoadAndKeep --genomeDir %s --readFilesIn %s --outFileNamePrefix aligned/%s_ --readFilesCommand zcat --outSAMtype BAM SortedByCoordinate --outSAMunmapped Within --outSAMattributes Standard" %(n_threads,star_ref,fqs,sample)
+    runstar="STAR --runThreadN %s --limitBAMsortRAM 10000000000 --genomeLoad LoadAndKeep --genomeDir %s --readFilesIn %s --outFileNamePrefix %s/%s_ --readFilesCommand zcat --outSAMtype BAM SortedByCoordinate --outSAMunmapped Within --outSAMattributes Standard" %(n_threads,star_ref,fqs,bam_path,sample)
     proc=subprocess.Popen(runstar.split(),
                           stdout=subprocess.PIPE,
                           stderr=subprocess.PIPE)
-    proc.wait()
-    return proc.returncode,proc.communicate()[1]
+    while proc.returncode == None:
+        proc.poll()
+        for keys,values in return_dict.items():
+            if len(values)==3:
+                if values[0]!=0:
+                    proc.kill()
+        time.sleep(1)
+            
+    else:   
+        proc.wait()
+        return_dict[sample]=[proc.returncode,proc.communicate()[1],runstar]
+        
+def run_star_par(n_threads,star_ref,fq_path,bam_path,samples):
+    pool = Pool()
+    func = partial(run_star, n_threads,star_ref,fq_path,bam_path)
+    pool.map(func, samples)
 
 
 def gather_starlogs(f):
@@ -76,7 +92,7 @@ def gather_starlogs(f):
     lines = a_file.readlines()
     if len(lines)<=2:
         return 0
-    if len(lines)==3 & lines[-1] == 'ALL DONE!\n':
+    if len(lines)==3 & (lines[-1] == 'ALL DONE!\n'):
         return 0
     else:
         l = 2 if lines[-1] == 'ALL DONE!\n' else 1
@@ -90,10 +106,10 @@ def run_fc(n_threads,gtf,bam):
                           stderr=subprocess.STDOUT)
     proc.wait()
 
-def run_fc_par(n_threads,gtf):
+def run_fc_par(n_threads,gtf,bam_path):
     pool = Pool()
     func = partial(run_fc, n_threads,gtf)
-    pool.map(func, glob("aligned/*.bam"))
+    pool.map(func, glob(f"{bam_path}/*.bam"))
     
 
 def fc2adata(p):
@@ -116,8 +132,7 @@ def main():
     console = Console()
     console.print("bulktools %s" %version,style="bold")
     args = parser.parse_args()
-    manager = Manager()
-    return_dict = manager.dict()
+    
     
     if which("STAR") is None:
         log.error("STAR not installed!")
@@ -140,30 +155,39 @@ def main():
         parser.print_help(sys.stderr)
         sys.exit(1)
 
-    n_threads = int(args.n_threads) if args.n_threads is not None else 1
     adata_out = args.adata_out if args.adata_out is not None else "adata_bulk_star.h5ad"
     fq_path = args.fq_path if args.fq_path is not None else "fastq"
+    bam_path = args.bam_path if args.bam_path is not None else "aligned"
     cpuCount = os.cpu_count()
     fastqs=glob(os.path.join(fq_path,"*.gz"))
     
+    if os.path.isdir(bam_path):
+        if os.listdir(bam_path):
+            log.error("Bam path is not empty! (path: %s)" %os.path.abspath(bam_path))
+            sys.exit(1)
+    
     if len(fastqs) == 0:
-        log.error("No fastq detected! (path: %s)" %fq_path)
+        log.error("No fastq detected! (path: %s)" %os.path.abspath(fq_path))
         sys.exit(1)
         
     samples = np.unique([os.path.basename(f).split("_")[0] for f in fastqs])
     n_samples = len(samples)
     
+    n_threads = int(args.n_threads) if args.n_threads is not None else int(np.floor(cpuCount/n_samples))
+    
     console.print("Pipeline parameters:", style="bold")
     console.print("fastq files path: %s" %os.path.abspath(fq_path))
+    console.print("bam files path: %s" %os.path.abspath(bam_path))
     console.print("STAR reference path: %s" %star_ref)
     console.print("GTF file path: %s" %gtf)
     console.print("Output adata path: %s" %os.path.abspath(adata_out))
     
     console.print("total CPU threads: %s" %cpuCount)
     if cpuCount < n_samples*int(n_threads):
-        console.print("WARNING: current settings will use more threads than available, reducing!",
+        console.print("WARNING: current settings will use more threads than available cores, reducing!",
                       style="bold orange3")
-    while cpuCount < n_samples*n_threads:
+
+    while (cpuCount < n_samples*n_threads) & (n_threads!=1):
         n_threads = n_threads-1
     console.print("Number of threads per sample: %s" %n_threads)
     console.print("With %s samples, peak utilization will use %s threads" %(n_samples,n_samples*n_threads))
@@ -177,7 +201,6 @@ def main():
     
     
     with console.status("[bold green]Counting number of reads...") as status:
-        return_dict = manager.dict()
         console.log("Loading reference")
         p1 = Process(target=ref_loader,args=(star_ref,))
         p1.start()
@@ -206,12 +229,6 @@ def main():
     
     console.log("Aligning reads")
     
-    def run_star_par(n_threads,star_ref,fq_path,samples):
-        pool = Pool()
-        func = partial(run_star, n_threads,star_ref,fq_path)
-        outs = pool.map(func, samples)
-        return_dict["run_star"] = dict(zip(samples,outs))
-
     with Progress(SpinnerColumn(),
             TextColumn("[progress.description]{task.description}"),
             BarColumn(),
@@ -225,41 +242,44 @@ def main():
         for sample,nreads in dct_nreads.items():
             dct_task[sample] = progress.add_task("[green]Aligning reads for sample %s..."%sample, 
                                             total=nreads)
-        #return_dict = manager.dict()
-        p3 = Process(target=run_star_par,args=(n_threads,star_ref,fq_path,samples))
-        p3.start()
-
-        #while len(glob("aligned/*.progress.out")) != len(glob(os.path.join(fq_path,"*.gz"))):
-        #    time.sleep(1)
         
-        while "run_star" not in return_dict:
+        p3 = Process(target=run_star_par,args=(n_threads,star_ref,fq_path,bam_path,samples))
+        p3.start()
+        
+        while not os.path.isdir(bam_path):
             time.sleep(1)
 
         prog = True
         err = False
-        errmsg=[]
-        while prog:                      
+        #errmsg=[]
+        while prog:
             for sample,task in dct_task.items():
-                if return_dict["run_star"][sample][0]!=0:
-                    progress.update(task,visible=False)
-                    err = True
-                    errmsg.append(return_dict["run_star"][sample][1].decode('utf-8'))
-                elif len(glob("aligned/*.progress.out")) != len(samples):
-                    progress.update(task,completed=gather_starlogs("aligned/%s_Log.progress.out" %sample))
-                    if len(glob("aligned/%s_Log.final.out" %sample))==1:
+                if sample in return_dict:
+                    if return_dict[sample][0]!=0:
+                        err = True
+                        errmsg=[sample,return_dict[sample][1].decode('utf-8'),return_dict[sample][2]]
+                elif len(glob(f"{bam_path}/*.progress.out")) == n_samples:
+                    progress.update(task,completed=gather_starlogs(f"{bam_path}/{sample}_Log.progress.out"))
+                    if len(glob(f"{bam_path}/{sample}_Log.final.out"))==1:
                         progress.update(task,completed=dct_nreads[sample])
             
             if err:
+                for sample,task in dct_task.items():
+                    progress.update(task,visible=False)
                 time.sleep(.1)
-                for e in errmsg:
-                    log.error(e)
+                log.error(f"Sample {errmsg[0]} experienced the following error:")
+                print(errmsg[2])
+                print(errmsg[1])
+                time.sleep(2)
+                p3.kill()
                 log.info("exiting...")
                 p3b = Process(target=ref_remover,args=(star_ref,))
                 p3b.start()
-                p3b.join()
-                sys.exit(0)
+                p3b.join()    
+                time.sleep(.1)              
+                sys.exit(1)
             
-            if len(glob("aligned/*final*")) == len(samples):
+            if len(glob(f"{bam_path}/*final*")) == len(samples):
                 prog = False
             progress.refresh()
             time.sleep(1)
@@ -272,7 +292,7 @@ def main():
         os.mkdir("fc")
 
     with console.status("[bold green]Counting features from alignments...") as status:
-        p4 = Process(target=run_fc_par,args=(n_threads,gtf,))
+        p4 = Process(target=run_fc_par,args=(n_threads,gtf,bam_path,))
         p4.start()
         p4.join()
 
